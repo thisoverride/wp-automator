91ac5600a06060f768dea876a5cb3dcc601128a8
thisoverride <n4yor93@hotmail.fr>
2024-05-17 01:16:24 +0200

    update dockercompose

--- src/service/DockerService.ts
+++ src/service/DockerService.ts
@@ -1,47 +1,50 @@
 import fs from 'node:fs';
-import { Request, Response } from 'express'; 
+import { Request, Response } from 'express';
 import path from 'path'
 
 export default class DockerService {
 
-public  async generateDocker(request:Request){
-  const {foldername,mysql_root_psswd,mysql_db,mysql_user,mysql_psswd} = request.body;
-  const folderPath = path.join(__dirname,'..','..','..',`/wp-sites/${foldername}`)
-  if(!fs.existsSync(folderPath)){
-    fs.mkdirSync(folderPath);
-    const replacements = {
-      '%example_root_password%': mysql_root_psswd,
-      '%wordpress%': mysql_db,
-      '%wordpress_user%':mysql_user,
-      '%example_password%':mysql_psswd
-    };
+  public async generateDocker(request: Request) {
+    const { foldername, mysql_root_psswd, mysql_user, mysql_psswd } = request.body;
+    console.log(request.body);
     
-    fs.readFile(`src/docker-compose.yml`, 'utf8',(err, data) => {
-      if (err) {
-        throw 'Erreur de lecture du fichier:' + err;
-    
-      }
+    const folderPath = path.join(__dirname, '..', '..', '..', `/wp-sites/${foldername}`)
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+      const replacements = {
+        '_{MYSQL_ROOT_PASSWORD}': mysql_root_psswd,
+        '_{MYSQL_USER}': mysql_user,
+        '_{MYSQL_PASSWORD}': mysql_psswd
+      };
 
-    //   // Remplacer les mots
-      let modifiedData = data;
-      for (const [oldWord, newWord] of Object.entries(replacements)) {
-        const regex = new RegExp(oldWord, 'g');
-        modifiedData = modifiedData.replace(regex, newWord);
-      }
-    
-      // Écrire le fichier modifié
-      fs.writeFile(`${folderPath}/docker-compose.yml`, modifiedData, 'utf8', (err) => {
+      fs.readFile(`src/docker-compose.yml`, 'utf8', (err, data) => {
         if (err) {
-          // console.error('Erreur d\'écriture dans le fichier:', err);
-          throw 'Erreur d\'écriture dans le fichier: \n' + err;
+          throw 'Erreur de lecture du fichier:' + err;
+
         }
-        return 'Le fichier a été modifié avec succès!';
+
+        //   // Remplacer les mots
+        let modifiedData = data;
+        for (const [oldWord, newWord] of Object.entries(replacements)) {
+          const regex = new RegExp(oldWord, 'g');
+          modifiedData = modifiedData.replace(regex, newWord);
+        }
+
+        // Écrire le fichier modifié
+        fs.writeFile(`${folderPath}/docker-compose.yml`, modifiedData, 'utf8', (err) => {
+          if (err) {
+            // console.error('Erreur d\'écriture dans le fichier:', err);
+            throw 'Erreur d\'écriture dans le fichier: \n' + err;
+          }
+          return 'Le fichier a été modifié avec succès!';
+        });
       });
-    });
+
+    }else {
+      throw new Error("Folder already exist");
+    }
 
   }
-  
-}
 
-// public getAllFolders() TODO
+  // public getAllFolders() TODO
 }
