1f52e30ceba5f60e53ce48e60ed1e9df37a7fa50
kanha-sydaphasavanh <k.sydaphasavanh@laposte.net>
2024-05-17 19:46:12 +0200

    make length limit for fields | add success response message

--- src/service/DockerService.ts
+++ src/service/DockerService.ts
@@ -1,13 +1,16 @@
 import fs from 'node:fs';
 import { Request, Response } from 'express';
 import path from 'path'
+import { promisify } from 'util';
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 export default class DockerService {
 
   public async generateDocker(request: Request) {
     const { foldername, mysql_root_psswd, mysql_user, mysql_psswd } = request.body;
     console.log(request.body);
-    
+
     const folderPath = path.join(__dirname, '..', '..', '..', `/wp-sites/${foldername}`)
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
@@ -17,31 +20,33 @@ export default class DockerService {
         '_{MYSQL_PASSWORD}': mysql_psswd
       };
 
-      fs.readFile(`src/docker-compose.yml`, 'utf8', (err, data) => {
-        if (err) {
-          throw 'Erreur de lecture du fichier:' + err;
+      try {
+        let data = await readFile(`src/docker-compose.yml`, 'utf8');
 
-        }
-
-        //   // Remplacer les mots
+        // Replace words
         let modifiedData = data;
         for (const [oldWord, newWord] of Object.entries(replacements)) {
           const regex = new RegExp(oldWord, 'g');
           modifiedData = modifiedData.replace(regex, newWord);
         }
 
-        // Écrire le fichier modifié
-        fs.writeFile(`${folderPath}/docker-compose.yml`, modifiedData, 'utf8', (err) => {
-          if (err) {
-            // console.error('Erreur d\'écriture dans le fichier:', err);
-            throw 'Erreur d\'écriture dans le fichier: \n' + err;
-          }
-          return 'Le fichier a été modifié avec succès!';
-        });
-      });
+        if (mysql_root_psswd.length < 8 || mysql_psswd.length < 8) {
+          throw new Error("Password should have at least 8 characters from both MySQL root and user");
+        }
 
-    }else {
-      throw new Error("Folder already exist");
+        if (mysql_user.length < 4) {
+          throw new Error("MySQL username should have at least 4 characters");
+        }
+        // Write the modified file
+        await writeFile(`${folderPath}/docker-compose.yml`, modifiedData, 'utf8');
+
+        return 'The file has been successfully generated!';
+      } catch (err) {
+        throw new Error('Error reading or writing the file: ' + err);
+      }
+
+    } else {
+      throw new Error("The folder already exist");
     }
 
   }
