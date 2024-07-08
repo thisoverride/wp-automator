ef42129c241050dbf6619cffc7334131ae991dec
Kanha Sydaphasavanh <k.sydaphasavanh@laposte.net>
2024-07-08 15:37:30 +0000

    modifie custom env variable to match wordpress env variable | update the script to retrieve env variable

--- .bin/wpcli_setup.sh
+++ .bin/wpcli_setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo "Loading..."
+set -e
+
+ENV_FILE="$(pwd)/.env"
+
+# Charger les variables d'environnement depuis le fichier .env
+if [ -f "$ENV_FILE" ]; then
+    export $(grep -v '^#' "$ENV_FILE" | xargs)
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+echo "Loading for setting up wordpress ..."
 sleep 5
 
 # Initialize attempt counter and set maximum attempts
@@ -7,16 +19,20 @@ attempt_counter_db=0
 max_attempts=10
 
 # Wait for the database to become available
-until docker-compose exec db mysqladmin ping -h "db" -u root -p${MYSQL_ROOT_PASSWORD} --silent > /dev/null 2>&1; do
+until docker-compose exec db mysqladmin ping -h "db" -u $WORDPRESS_DB_USER -p$MYSQL_ROOT_PASSWORD --silent > /dev/null 2>&1; do
   attempt_counter_db=$((attempt_counter_db + 1))
   echo "Waiting for the database to become available... Attempt $attempt_counter_db of $max_attempts"
   
-  if [ $attempt_counter_db -eq $max_attempts ]; then
+  if [ "$attempt_counter_db" -eq "$max_attempts" ]; then
     echo "Database did not become available after $max_attempts attempts. Exiting."
     exit 1
   fi
-  
-  sleep 10 # Adjust sleep as necessary
+
+  # Adjust sleep as necessary
+  sleep 20 || {
+    echo "Sleep command failed. Exiting."
+    exit 1
+  } 
 done
 
 # Reset attempt counter for WordPress check
@@ -32,50 +48,65 @@ until [ $(docker-compose exec -T wordpress curl --output /dev/null --silent --he
       exit 1
     fi
     
-    sleep 2
+    sleep 10 || {
+    echo "Sleep command failed. Exiting."
+    exit 1
+  } 
 done
 
+
+ADDONS="%{ADDONS}"
 # Execute all wp-cli commands in a single container session
 docker-compose run --rm --user="root" wpcli bash -c "
+    # Define a function to wrap wp-cli commands with --allow-root
+    wp() {
+        command wp \"\$@\" --allow-root
+    }
+
     # Check if WordPress core files exist
     if [ -f '/var/www/html/wp-config.php' ]; then
-        echo 'WordPress files detected.'
+        echo 'WordPress files detected.';
     else
-        echo 'WordPress files not detected. Downloading WordPress...'
-        wp core download --path='/var/www/html' --allow-root;
+        echo 'WordPress files not detected. Downloading WordPress...';
+        wp core download --path='/var/www/html';
     fi
 
     # Check if WordPress is installed
-    if wp core is-installed --allow-root 2>/dev/null; then
-      echo 'WordPress is already installed.'
+    if wp core is-installed 2>/dev/null; then
+        echo 'WordPress is already installed.';
     else
-      echo 'Installing WordPress...'
-      wp core install --path='/var/www/html' --url='http://${WP_HOST}:${WP_PORT}' --title='${WP_PROJECT_NAME}' --admin_user='${WP_USER}' --admin_password='${WP_PASSWORD}' --admin_email='${WP_EMAIL}' --allow-root;
+        echo 'Installing WordPress...'
+        wp core install --path='/var/www/html' --url='http://$WP_HOST:$WP_PORT' --title=\"$PROJECT_NAME\" --admin_user=\"$WP_USER\" --admin_password=\"$WP_PASSWORD\" --admin_email=\"$WP_EMAIL\";
     fi
 
     # Language setup
-    echo 'Setting up the language...'
-    wp language core install ${WP_LANGUAGE} --allow-root;
-    wp language core activate ${WP_LANGUAGE} --allow-root;
+    echo 'Setting up the language...';
+    wp language core install \"$WP_LANGUAGE\";
+    wp site switch-language \"$WP_LANGUAGE\";
 
-    # Install and activate the WooCommerce plugin
-    echo 'Install addons...'
-    wp plugin install %{ADDONS} --activate --allow-root;
+    # Check if ADDONS is not empty
+    if [ -z \"$ADDONS\" ]; then
+        echo 'No plugins to install...';
+    else
+        echo 'Installing plugins...';
+        wp plugin install $ADDONS --activate;
+    fi
+
+    # Check if JWT Authentication for WP REST API plugin is installed and set jwt secret key
+    if wp plugin is-installed jwt-authentication-for-wp-rest-api; then
+        wp config set JWT_AUTH_SECRET_KEY \"$SECRET_KEY\";
+    fi
 
     # Set the permalink structure
     echo 'Setting the permalink structure...'
-    wp rewrite structure '/%postname%/' --allow-root;
+    wp rewrite structure '/%postname%/';
 
-    # Flush rewrite rules | The command bellow already flush the rewrite rules but it's good to have it here :)
+    # Flush rewrite rules | The command above already flush the rewrite rules but it's good to have it here :)
     echo 'Flushing rewrite rules...'
-    wp rewrite flush --allow-root;
-"
+    wp rewrite flush;
+";
 
-docker-compose exec wordpress bash -c '
-  chmod 644 ./wp-config.php && \
-  echo "define('\''JWT_AUTH_SECRET_KEY'\'', '\''${SECRET_KEY}'\'');" >> ./wp-config.php && \
-  chmod 444 ./wp-config.php;
-
-  chown -R www-data:www-data /var/www/html;
-  echo 'File permission changed';
-'
+docker-compose exec wordpress bash -c "
+    chown -R www-data:www-data /var/www/html;
+    echo 'File permission changed';
+    ";
